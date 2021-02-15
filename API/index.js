4ab1047eebedceda93b955e1c7f5e2bc670928e3
ksoka <t9soka00@students.oamk.fi>
2021-02-15 18:42:27 +0200

    getting works, posting doesn't

--- API/index.js
+++ API/index.js
@@ -3,13 +3,42 @@ const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const { v4: uuidv4 } = require('uuid'); 
+const has = require('has-value');
 
-app.use(cors());
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
+let userData = {
+  Users: [
+  {
+    id: uuidv4(),
+    firstName: 'Alice',
+    lastName: 'Smith',
+    email: 'alice.smith@gmail.com',
+    dateOfBirth: '1997-10-31',
+  },
+  {
+    id: uuidv4(),
+    firstName: 'Kalle',
+    lastName: 'Soukka',
+    email: 't9soka00@students.oamk.fi',
+    dateOfBirth: '1998-2-14',
+  },
+  {
+    id: uuidv4(),
+    firstName: 'Felix',
+    lastName: 'Pape',
+    email: 'pape.felix@oamk.fi',
+    dateOfBirth: '1996-15-2'
+  }
+  ]
+}
 
 app.route('/Posts')
     .get((req, res) => res.send('get Posts'))
@@ -20,12 +49,44 @@ app.route('/Posts/{postId}')
     .patch((req, res) => res.send('patch single Posts'))
     .delete((res, req) => res.send('delete single Posts')) 
 
+    
+app.route('/User')
+    .get((req, res) => { res.json(userData)})
+    .post((req, res) => {
 
-app.route('User')
-    .post((req, res) => res.send('post new user'))
+      const newUser = {
+        id: uuidv4(),
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        dateOfBirth: req.body.dateOfBirth
+      };
 
-app.route('Users/{userId}')
-    .get((req, res) => res.send('get single Users'))
+      userData.Users.push(newUser);
+
+      res.status(201);
+      res.json(newUser);
+    });
+
+app.route('User/:userId')
+    .get((req, res) => {
+      const resultUser = userData.Users.find(u => {
+        if (u.id == req.params.userId) {
+          return true;
+        }
+        else {
+          return false;
+        }
+      });
+      if(resultUser === undefined)
+      {
+        res.sendStatus(404)
+      }
+      else
+      {
+        res.json(resultUser);
+      }
+    })
     .patch((req,res) => res.send('patch sigle Users'))
     
 
