07983533a146b6246c56ee3c0b6ef4057d1c8850
ksoka <t9soka00@students.oamk.fi>
2021-02-15 14:20:14 +0200

    some set up with express

--- API/index.js
+++ API/index.js
@@ -1,10 +1,33 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.route('/Posts')
+    .get((req, res) => res.send('get Posts'))
+    .post((req, res) => res.send('post Posts'))
+
+app.route('/Posts/{postId}')
+    .get((req, res) => res.send('get single Posts'))
+    .patch((req, res) => res.send('patch single Posts'))
+    .delete((res, req) => res.send('delete single Posts')) 
+
+
+app.route('User')
+    .post((req, res) => res.send('post new user'))
+
+app.route('Users/{userId}')
+    .get((req, res) => res.send('get single Users'))
+    .patch((req,res) => res.send('patch sigle Users'))
+    
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
