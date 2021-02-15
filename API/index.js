d49ea5b0ecbe294261b46be1c3770b1f40a440d0
ksoka <t9soka00@students.oamk.fi>
2021-02-15 20:33:56 +0200

    getting and posting works for everything

--- API/index.js
+++ API/index.js
@@ -13,7 +13,26 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-
+let postData = {
+  Post: [
+    {
+      postId: uuidv4(),
+      title: 'Mountain Bike',
+      description: 'This is a high quality mountain bike for extreme condition biking',
+      category: 'Outdoor Sports',
+      location: 'Oulu FI',
+      images1: 'Bike.png',
+      images2: 'Bike2.png',
+      images3: '',
+      images4: '',
+      askingPrice: 300,
+      deliveryType: 'Shipping',
+      dateOfPosting: '15-2-2021',
+      sellerName: 'Kalle Soukka',
+      sellerInfo: 'Schoolstreet 2'
+    }
+  ]
+}
 let userData = {
   Users: [
   {
@@ -41,11 +60,49 @@ let userData = {
 }
 
 app.route('/Posts')
-    .get((req, res) => res.send('get Posts'))
-    .post((req, res) => res.send('post Posts'))
+    .get((req, res) => { res.json(postData)})
+    .post((req, res) => {
+      const newPost = {
+        postId: uuidv4(),
+        title: req.body.title,
+        description: req.body.description,
+        category: req.body.category,
+        location: req.body.location,
+        images1: req.body.images1,
+        images2: req.body.images2,
+        images3: req.body.images3,
+        images4: req.body.images4,
+        askingPrice: req.body.askingPrice,
+        deliveryType: req.body.deliveryType,
+        dateOfPosting: req.body.dateOfPosting,
+        sellerName: req.body.sellerName,
+        sellerInfo: req.body.sellerInfo
+      };
+      postData.Post.push(newPost);
 
-app.route('/Posts/{postId}')
-    .get((req, res) => res.send('get single Posts'))
+      res.status(201);
+      res.json(newPost);
+    })
+
+app.route('/Posts/postsId')
+    .get((req, res) => {
+      const resultPost = postData.Post.find(p => {
+        if (p.postId == req.params.postsId) {
+          return true;
+        }
+        else {
+          return false;
+        }
+      })
+      if(resultPost === undefined)
+        {
+          res.sendStatus(404)
+        } 
+        else
+        {
+          res.json(resultPost);
+        }
+    })
     .patch((req, res) => res.send('patch single Posts'))
     .delete((res, req) => res.send('delete single Posts')) 
 
